fba2edfdf97c9864b4094254a723874a66648c07
Your Name <trynia78@gmail.com>
2025-01-04 18:39:41 +0700

    04 - 01 - 25

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,66 @@
-import '@styles/homepage.css'
-import '@styles/reset.css'
-import Head from 'next/head'
+import '@styles/reset.css';
+import '@styles/wleo.css';
+import '@styles/homepage.css';
+import Head from 'next/head';
 
 const Home = () => {
   return (
     <>
       <Head>
-        <title>KAORI - Home</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+        <title>Nusantaku - Anime News</title>
       </Head>
-      <div>
-        {/* Navigation Bar Section */}
-        <nav>
-          <div className="nav-logo">
-            <h1 className="notranslate">KAORI</h1>
-          </div>
-
-          <div className="nav-menu">
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Latest News</a></li>
-              <li><a href="#">Anime Reviews</a></li>
-            </ul>
-          </div>
-
-          <div className="nav-dropdown-menu">
-            <input id="check" type="checkbox" />
-            <label htmlFor="check" className="hamburger">
-              <span></span>
-              <span></span>
-              <span></span>
-            </label>
-            <ul>
-              <li><a href="#">Home</a></li>
-            </ul>
-          </div>
-
-          <div className="nav-search">
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </div>
-        </nav>
-
-        <div className="container">
-          {/* Latest News Section */}
-          <section className="latest-news">
-            <h2 className="title-section">Latest News</h2>
-            <div className="wrapper-latest-news">
-              <a href="#" className="news one">
-                <div className="image">
-                  <img src="./media/bg-image.jpg" alt="" />
-                </div>
-                <div className="content">
-                  <p className="label"><span>KAORI</span> - 11 April 2024</p>
-                  <h2 className="title">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-                  <p className="overflow-hd">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores id aliquam dignissimos impedit a. Optio sequi ipsa natus fugit impedit iusto pariatur vero?
-                  </p>
-                  <p id="submit-btn">read more <span>⟶</span></p>
-                </div>
-              </a>
-            </div>
-          </section>
+      <div className="banner">
+        <h1 className="banner-title">Yoimiya Adalah Wanita Tercantik Aku Sangat Suka</h1>
+      </div>
+      <nav>
+        <div className="nav-logo">
+          <img src="https://raw.githubusercontent.com/TabawaX/waifudb/refs/heads/master/wdb/20250101_153150.png" alt="Logo Nusantaku" />
         </div>
+        <div className="nav-menu">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Latest News</a></li>
+          </ul>
+        </div>
+        <div className="nav-dropdown-menu">
+          <input id="check" type="checkbox" />
+          <label htmlFor="check" className="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Latest News</a></li>
+          </ul>
+        </div>
+        <div className="nav-search">
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
+      </nav>
+      <div className="container">
+        <section className="latest-news">
+          <h2 className="title-section">Latest News</h2>
+          <div className="wrapper-latest-news">
+            <a href="#" className="news one">
+              <div className="image">
+                <img src="https://raw.githubusercontent.com/TabawaX/waifudb/refs/heads/master/docs/news/sample_cf89a185dd8173f029a245ecc7b5ef6858e6f51d.jpg" alt="" />
+              </div>
+              <div className="content">
+                <p className="label"><span>nusantaku</span> - 11 April 2024</p>
+                <h2 className="title">Titleny.</h2>
+                <p className="overflow-hd">Deskripsi</p>
+                <p id="submit-btn">read more <span>⟶</span></p>
+              </div>
+            </a>
+          </div>
+        </section>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
